f3f748a4ab5d25eb08d67c0540a8115f5c17ad24
M1ngX1ao <97147464+m1ngx1ao@users.noreply.github.com>
2023-10-28 19:03:59 +0200

    Junior 2 refactort

--- junior2/src/bild_decoder.py
+++ junior2/src/bild_decoder.py
@@ -1,21 +1,13 @@
 import PIL.Image
 
 def decode(bild: PIL.Image.Image) -> str:
-	loesungssatz = ''
+	ergebnis = ''
 	p = (0, 0)
 	old_p = ()
 	while p != old_p:
-		#j'ai un point p - je dois regarder ce qu'il y a dedans
-		char, px, py, *_ = bild.getpixel(p)
-		#trouve la lettre
-		loesungssatz += chr(char)
-		#va au prochain
-		old_px, old_py = p
-		p = (old_px + px) % bild.width, (old_py + py) % bild.height
-		old_p = (old_px, old_py)
-	return loesungssatz
-
-
-
-def __hole_naechste_infos(x: int, y: int) -> tuple[str, tuple[int, int]]:
-	return ('s', (x, y))
+		char, dx, dy, *_ = bild.getpixel(p)
+		ergebnis += chr(char)
+		old_p = p
+		px, py = p
+		p = (px + dx) % bild.width, (py + dy) % bild.height
+	return ergebnis
